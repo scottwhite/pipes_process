3f52c7bf42e16812eda4d32018ada610a83e90a5
Scott White <scott@3rdarm.com>
2017-08-10 00:04:30 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 /*jshint evil: true, boss: true, node: true*/
 module.exports = {
   Creds:{
-      sid: 'AC584a27ef4e2a013398ad27b5bcdb16a3',
-      auth_token: '5602b73423d50848001fb55d719c6c8a'
+      sid: 'bacon',
+      auth_token: 'pants'
   },
   process_url: {
     dev: 'http://localhost:3000',
